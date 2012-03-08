08fd7dbf2b57e98cd1ee95cdf9589247a131c1fd
Ray Bao <baoruihan@gmail.com>
2012-03-08 15:37:30 +0900

    first page

---
+++ django_bookmarks/bookmarks/views.py
@@ -0,0 +1,18 @@
+# Create your views here.
+from django.http import HttpResponse
+
+def main_page(request):
+    output = u'''
+		<html>
+			<head><title>%s</title></head>
+			<body> 
+				<h1>%s</h1><p>%s</p>
+			</body>
+		</html>
+	''' % (
+		u'Django Bookmarks',
+		u'welcome to Django Bookmarks',
+		u'Where you can store and share bookmarks!'
+	)
+    return HttpResponse(output)
+        
